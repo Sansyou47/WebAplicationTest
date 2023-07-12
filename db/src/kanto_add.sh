494da6c0dbf497f98b9258d37a31484cf103912e
Kazuhiro Yomesaka <yomesaka.ikkousen@gmail.com>
2023-07-12 10:27:32 +0900

    作成

---
+++ db/src/kanto_add.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mysql -u myuser -p -f < kanto.sql
---
+++ web/src/0526/get.php
@@ -0,0 +1,18 @@
+<html>
+
+<head>
+    <title>GETメソッドでのフォーム受信</title>
+</head>
+
+<body>
+    <h1>GETメソッドでのフォーム受信</h1>
+
+    受信内容：
+
+    <?php
+    print  $_GET['MESSAGE'];
+    ?>
+
+</body>
+
+</html>
