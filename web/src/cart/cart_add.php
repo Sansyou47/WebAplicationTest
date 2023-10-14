1b1d40e9f75bda793b7a74502a3c35ebd267a554
Kazuhiro Yomesaka <yomesaka.ikkousen@gmail.com>
2023-10-14 19:48:32 +0900

    環境設定

--- web/src/cart/cart_add.php
+++
@@ -1,9 +0,0 @@
-<?php
-$ident = $_POST['ident'];
-$quantity = $_POST['quantity'];
-
-require_once __DIR__ . '/../classes/cart.php';
-$cart = new Cart();
-$cart->addItem($ident, $quantity);
-
-require_once __DIR__ . '/cart_list.php';
--- web/src/cart/cart_delete.php
+++
@@ -1,6 +0,0 @@
-<?php
-$ident = $_POST['ident'];
-require_once __DIR__ . '/../classes/cart.php';
-$cart = new Cart();
-$cart->deleteItem($ident);
-require_once __DIR__ . '/cart_list.php';
