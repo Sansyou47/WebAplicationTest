829efade1493877c6a061bc998d6652f485a7734
Kazuhiro Yomesaka <yomesaka.ikkousen@gmail.com>
2023-10-16 12:02:49 +0900

    データベースの初期化

--- db/src/init.sql
+++ db/src/init.sql
@@ -2,7 +2,7 @@ create table test(
     number  int auto_increment primary key,
     name    varchar(50) not null,
     age     int
-)
+);
 
 insert into test(name, age) values('大阪太郎',50);
 insert into test(name, age) values('東京花子',50);
